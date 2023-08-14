29719a328e4c777bca1c84e4385ed681520049d2
yuliia <pustovayajulia@gmail.com>
2023-08-14 16:32:46 +0300

    Add outputCallback

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const parse = (data) => JSON.parse(data);
 const resultArr = [];
 let countOfReadFile = 0;
 
-function requesrData(method, URL, cb) {
+function requesrData(method, URL, cb, cb2) {
   const xml = new XMLHttpRequest();
   xml.open(method, URL);
   xml.send();
@@ -11,20 +11,24 @@ function requesrData(method, URL, cb) {
     if (xml.readyState === 4 && xml.status < 400) {
       const response = parse(xml.response);
       cb(response);
-      countOfReadFile++;
-      if (countOfReadFile === 2) {
-        console.log(resultArr);
-      }
+      cb2();
     }
   });
 }
 
-function renderElements(response) {
+function addElementsInArr(response) {
   response.children.forEach(function (el) {
     resultArr.push(el);
   });
 }
 
-requesrData("GET", 'files/data.json', renderElements);
-requesrData("GET", 'files/data2.json', renderElements);
+function outPutElements(response) {
+  countOfReadFile++;
+  if (countOfReadFile === 2) {
+    console.log(resultArr);
+  }
+}
+
+requesrData("GET", 'files/data.json', addElementsInArr, outPutElements);
+requesrData("GET", 'files/data2.json', addElementsInArr, outPutElements);
 
